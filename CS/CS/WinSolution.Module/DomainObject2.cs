7d324ad5ca77e0d5c7e0ac6f4ccd7b5c85ba0039
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-05-20 02:47:26 +0300

    initial commit

---
+++ CS/CS/WinSolution.Module/DomainObject2.cs
@@ -0,0 +1,16 @@
+using System;
+
+using DevExpress.Xpo;
+
+using DevExpress.ExpressApp;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+
+namespace WinSolution.Module {
+    [DefaultClassOptions]
+    public class DomainObject2 : BaseObject {
+        public DomainObject2(Session session) : base(session) { }
+    }
+
+}
